a4d13f9cffcb2fc6ffa787879e662826812403e2
Olliecad1 <officialgamermorris@gmail.com>
2017-03-22 17:56:34 +0000

    Add files via upload

---
+++ block1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+## Import the modules required
+import bitcoin
+import bitcoin.rpc
+from twython import Twython
+import os
+
+## Create a proxy object and connect to the bitcoin.rpc
+myproxy = bitcoin.rpc.Proxy()
+
+## Get the latest CBlock data from bitcoin rpc proxy
+block_info = myproxy.getblock(myproxy.getblockhash(myproxy.getblockcount()))
+
+#Setting these as variables will make them easier for future edits
+app_key =  ''
+app_secret = ''
+oauth_token = ''
+oauth_token_secret = ''
+
+#Prepare your twitter, you will need it for everything
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+#The above should just be a single line, without the break
+
+#/home/pi/Music/first1.py is the path location of the file to run it
+os.system('python /home/pi/Music/first1.py')
+
+p2 = "TXVol: "
+
+p3 = len(block_info.vtx)
+
+U = p2 + str(p3)
+
+string = bitcoin.core.b2lx(block_info.GetHash());
+
+b1 = "Block Hash: "
+
+O = b1 + str(string)
+
+a = "CBlock Object Info: Height "
+
+w = myproxy.getblockcount()
+
+P = a + str(w)
+
+
+s = twitter.update_status(status='New Block: ' + P + "\n" + O + "\n" + U)
+print (s)
+
+
+
+
